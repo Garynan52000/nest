34094736c548ea4446b25f2df198c1420ef8e4a7
Garynan52000 <2885354314@qq.com>
2020-06-09 19:39:00 +0800

    完成 基础 module 学习

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
 import { AccountController } from './account/account.controller';
-import { CatsService } from './cats/cats.service';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
+  imports: [CatsModule],
   controllers: [
     AppController, 
-    CatsController, 
     AccountController
   ],
   providers: [
-    AppService, 
-    CatsService
+    AppService
   ],
 })
 export class AppModule {}
